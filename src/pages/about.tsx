b68d5469f0e36ec188d1c1dfeef31e36e71c86a8
abel <abel249733@gmail.com>
2022-12-23 02:27:00 -0600

    finnished finnal draft need opinnion

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,67 +6,71 @@ export { Head } from "../components/layout";
 export default function Page() {
     return (
         <Layout title="About OurVoice Technologies" headerText="About OurVoice Technologies">
-            <div className="center" style={{ "paddingLeft": "20%", "paddingRight": "20%"}}>
-               
-                <div className="employes" style={{ "paddingLeft": "5%", "paddingRight": "0%"}}>
-                <div className=".employe">
-                    <img src="" alt="icon" width="500px" height="500px"/>
-                    <h3>Carson</h3>
-                    <h4>CEO of OurVoice Technologies</h4>
-                </div>
-                <div className=".employe">
-                    <p>insert bio</p>
-                </div>
-                <div className=".employe">
-                    <img src="" alt="icon" width="500px" height="500px"/>
-                    <h3>Mathew MCcoy</h3>
-                    <h4>CMO of OurVoice Technologies</h4>
-                </div>
-                <div className=".employe">
-                    <p>insert bio</p>
-                </div>
-                <div className=".employe">
-                    <img src="" alt="icon"></img>
-                    <h3>Abel Semahegn</h3>
-                    <h4>CTO of OurVoice Technologies</h4>
-                </div>
-                <div className=".employe">
-                    <p>insert bio</p>
-                </div>
-                <div className=".employe">
-                    <img src="" alt="icon" width="500px" height="500px"/>
-                    <h3>Aidan Williams</h3>
-                    <h4>insert title</h4>
-                </div>
-                <div className=".employe">
-                    <p>insert bio</p>
-                </div>
-                <div className=".employe">
-                    <img src="" alt="icon" width="500px" height="500px"/>
-                    <h3>Rayyan Choudhry</h3>
-                    <h4>insert title</h4>
-                </div>
+            <div className="center" style={{ "paddingLeft": "10%", "paddingRight": "10%" }}>
 
-                <div className=".employe">
-                    <p>insert bio</p>
-                </div>
-                <div className=".employe">
-                    <img src="" alt="icon" width="500px" height="500px"/>
-                    <h3>Stephen</h3>
-                    <h4>insert title</h4>
-                </div>
-                <div className=".employe">
-                    <p>insert bio</p>
-                </div>
-                
-        
-                
-            </div>
-            <h1>Who We Are</h1>
-                <p>OurVoice is a one-of-a-kind Edtech Company. We give students a voice unrivaled by any other product on the market.</p>
+                <div className="employes" style={{ "paddingLeft": "5%", "paddingRight": "0%" }}>
+                    <div className=".employe">
+                        <img src="" alt="icon" width="500px" height="500px" />
+                        <h3>Carson</h3>
+                        <h4>CEO of OurVoice Technologies</h4>
+                    </div>
+                    <div className=".employe">
+                        <p>insert bio</p>
+                    </div>
+                    <div className=".employe">
+                        <img src="" alt="icon" width="500px" height="500px" />
+                        <h3>Mathew MCcoy</h3>
+                        <h4>CMO of OurVoice Technologies</h4>
+                    </div>
+                    <div className=".employe">
+                        <p>insert bio</p>
+                    </div>
+                    <div className=".employe">
+                        <img src="" alt="icon"></img>
+                        <h3>Abel Semahegn</h3>
+                        <h4>CTO of OurVoice Technologies</h4>
+                    </div>
+                    <div className=".employe">
+                        <p>insert bio</p>
+                    </div>
+                    <div className=".employe">
+                        <img src="../images/icon.png" alt="icon" width="500px" height="500px" />
+                        <h3>Aidan Williams</h3>
+                        <h4>insert title</h4>
+                    </div>
+                    <div className=".employe">
+                        <p>insert bio</p>
+                    </div>
+                    <div className=".employe">
+                        <img src="" alt="icon" width="500px" height="500px" />
+                        <h3>Rayyan Choudhry</h3>
+                        <h4>insert title</h4>
+                    </div>
+                    <div className=".employe">
+                        <p>insert bio</p>
+                    </div>
 
-                <h1>How We Started</h1>
-                <p>OurVoice Technologies was started with one simple mission: To empower students with their own voices. Students often feel unheard or have no way to speak their minds. At OurVoice we want to change that.</p>
+                    <div className=".employe">
+                        <img src="" alt="icon" width="500px" height="500px" />
+                        <h3>Stephen</h3>
+                        <h4>insert title</h4>
+                    </div>
+                    <div className=".employe">
+                        <p>insert bio</p>
+                    </div>
+
+                </div>
+                <div className="texts">
+                    <div className="text">
+                        <h1>Who We Are</h1>
+                        <p>OurVoice is a one-of-a-kind Edtech Company. We give students a voice unrivaled by any other product on the market.</p>
+                    </div>
+
+                    <div className="text">
+                        <h1>How We Started</h1>
+                        <p>OurVoice Technologies was started with one simple mission: To empower students with their own voices. Students often feel unheard or have no way to speak their minds. At OurVoice we want to change that.</p>
+                    </div>
+                </div>
             </div>
         </Layout>
     );
--- src/pages/ourvoiceforstudents.tsx
+++ src/pages/ourvoiceforstudents.tsx
@@ -45,16 +45,19 @@ export default function Page() {
                 
                 <ul>              
                 <h3>According to research at the Quaglia Institute for School Voice and Aspirations (QISVA)</h3>
-                <li>can give your students a voice for less than a quarter of the price of a single #2 pencil. 
-                More than ⅔ of students feel uncomfortable sharing suggestions or concerns directly with their teachers<a href="https://www.quagliainstitute.org/uploads/legacy/Student_Voice_Grades_6-12_Decade_of_Data_Report.pdf">(QISVA)</a>. </li>
+                <p>can give your students a voice for less than a quarter of the price of a single #2 pencil. 
+                More than ⅔ of students feel uncomfortable sharing suggestions or concerns directly with their teachers<a href="https://www.quagliainstitute.org/uploads/legacy/Student_Voice_Grades_6-12_Decade_of_Data_Report.pdf">(QISVA)</a>. </p>
                 </ul>
                 </div>
 
                 <div className="text">
-                <h3>Cost</h3>
-                <p> At OurVoice we believe quality software shouldn’t break the bank. That's why we only charge 8 cents a month for student accounts.</p>
-                <h3>Customer Care</h3>
-                <p>We want to make sure each school district is in good hands that's why we have a sales team that works around the clock to provide the best experience for our customers. Each customer care officer will work directly with your district to set up, and we don't stop there, if you ever have a problem our team will be there for you. At OurVoice we take care of you. </p>
+                <h3>Why Choose Us</h3>
+
+                <ul>
+                    <li>    OurVoice for students was built with students in mind.</li>
+                    <li>We allow schools to both give their students a voice and collect information that will improve the student experience.</li>
+                    <li>OurVoice Technologies was created to give students a voice and OurVoice For Students does just that. </li>
+                </ul>
                 </div>
 
                 <div className="text">
@@ -69,18 +72,29 @@ export default function Page() {
                     <li>Our dynamic poll systems allow teachers and admins the ability to post polls and ask questions to as many or as few students as they would like, allowing them to collect data on everything from a classroom to a district. </li>
                 </ul>
                 </div>
-   
- 
-                <div className="text">
-                <h3>Why Choose Us</h3>
 
+                <div className="text">
+                <h3>Cost</h3>
+                <p> At OurVoice we believe quality software shouldn’t break the bank. That's why we only charge 8 cents a month for student accounts.</p>
+                <h3>Customer Care</h3>
+                <p>We want to make sure each school district is in good hands that's why we have a sales team that works around the clock to provide the best experience for our customers. Each customer care officer will work directly with your district to set up, and we don't stop there, if you ever have a problem our team will be there for you. At OurVoice we take care of you. </p>
+                </div>
+
+
+   
+                <div className="text">
+                <h3>Features</h3>
                 <ul>
-                    <li>    OurVoice for students was built with students in mind.</li>
-                    <li>We allow schools to both give their students a voice and collect information that will improve the student experience.</li>
-                    <li>OurVoice Technologies was created to give students a voice and OurVoice For Students does just that. </li>
+                    <li>    OurVoice for students is custom fit for schools. Schools can change settings to best fit their student body.
+                    Schools host OurVoice on their own platform allowing them increased customizability, ease of use, and security.
+                    </li>
+                    <br />
+                    <li>Teachers and admins can create custom VoiceBoxes, about eh3 verything from safety to curriculum. These VoiceBoxes allow students to give feedback and insight that can improve teaching, safety, and the overall student experience.</li>
+                    <br />
+                    <li>Our dynamic poll systems allow teachers and admins the ability to post polls and ask questions to as many or as few students as they would like, allowing them to collect data on everything from a classroom to a district. </li>
                 </ul>
                 </div>
-            </div>
+                </div>
         </Layout>
     );
 }

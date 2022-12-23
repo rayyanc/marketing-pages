77a46afbc27b60a35a0874c0e247a0dc84420938
rayyan choudhry <rayyan.choudhry@student.allenisd.org>
2022-12-23 14:33:49 -0600

    quotes-changed

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,15 +2,15 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `marketing`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: "marketing",
+    siteUrl: "https://www.yourdomain.tld"
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: ["gatsby-plugin-mdx", {
-    resolve: 'gatsby-source-filesystem',
+    resolve: "gatsby-source-filesystem",
     options: {
       "name": "pages",
       "path": "./src/pages/"
